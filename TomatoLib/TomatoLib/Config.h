90fd6d05fe21857c859e658b5a1414d34320de40
Alex Brouwer <a.brouwer@locuz.nl>
2018-02-18 04:50:40 +0100

    cleaned up config

--- TomatoLib/TomatoLib/Config.h
+++ TomatoLib/TomatoLib/Config.h
@@ -2,17 +2,25 @@
 #define __TL_CONFIG__
 
 // if you want to use old opengl versions without fancy buffers
-// #define TL_OPENGL_OLD
+#ifndef TL_OPENGL_OLD
+//#define TL_OPENGL_OLD
+#endif
 
-// extentions require external libaries, define these if you desire in your build script or uncomment them
-// #define TL_ENABLE_VTFLIB
-// #define TL_ENABLE_FTGL
+// extentions require external libaries, define these if you desire in your build script
+#ifndef TL_ENABLE_FTGL
+//#define TL_ENABLE_FTGL
+#endif
 
-#ifdef LINUX
-#define TL_OPENGL_OLD
-#define TL_ENABLE_EGL
-#else
-#define TL_ENABLE_GLFW
+#ifndef TL_ENABLE_VTFLIB
+//#define TL_ENABLE_VTFLIB
+#endif
+
+#ifndef TL_ENABLE_SSL
+//#define TL_ENABLE_SSL
+#endif
+
+#ifndef TL_ENABLE_EGL
+//#define TL_ENABLE_EGL
 #endif
 
 #endif
